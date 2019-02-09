dce76caede993668e72221a58a1ca1922efd8926
vitcmaestro <40243064+vitcmaestro@users.noreply.github.com>
2019-02-09 13:22:24 +0530

    Update alter_valid_or_not.py

--- alter_valid_or_not.py
+++ alter_valid_or_not.py
@@ -1,15 +1,17 @@
 import collections
+import math
 s = input("")
 freq = collections.Counter(s)
 maxer = 0
 ltr = s[0]
+mid = math.ceil(len(s)//2)
 for x,y in freq.items():
     if(y>maxer):
         maxer = y
         ltr = x
 if(len(s) == 1):
     print("yes")
-elif(y> (len(s)//2)):
+elif(y>= mid):
     print("no")
 else:
     print("yes")
