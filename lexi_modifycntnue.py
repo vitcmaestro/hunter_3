71e36d16248f7478c424cd1217932a6572184d30
vitcmaestro <40243064+vitcmaestro@users.noreply.github.com>
2019-02-12 15:24:04 +0530

    Update lexi_modifycntnue.py

--- lexi_modifycntnue.py
+++ lexi_modifycntnue.py
@@ -2,13 +2,11 @@ s = input("")
 lis = list(s)
 d = 0
 for i in range(-1,-(len(s)),-1):
-    if(ord(lis[i])>ord(lis[i-1])):
-        lis[i],lis[i-1] = lis[i-1],lis[i]
-        d = 1
-        break
-else:
-    d = 0
+    for j in range(i-1,-(len(s)),-1):
+        if(ord(lis[i])>ord(lis[j])):
+            lis[i],lis[j] = lis[j],lis[i]
+            d = 1
+            print("".join(map(str,lis)))
+            exit(0)
 if(d == 0):
     print("-1")
-else:
-    print("".join(map(str,lis)))
