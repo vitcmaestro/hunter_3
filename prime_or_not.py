29f1cc4cdf483a97c688588c6e8e0cd308b020ef
vitcmaestro <40243064+vitcmaestro@users.noreply.github.com>
2019-02-12 15:54:50 +0530

    Create prime_or_not.py

---
+++ prime_or_not.py
@@ -0,0 +1,20 @@
+import math
+def isprime(x):
+    if(x ==2):
+        return True
+    elif(x%2 == 0):
+        return False
+    else:
+        for j in range(2,int(math.sqrt(x)+1)):
+            if(x%j == 0):
+                return False
+        return True
+n = int(input(""))
+prime = []
+for i in range(2,n):
+    if(isprime(i)):
+        prime.append(i)
+if(len(prime)>0):
+    print(" ".join(map(str,prime)))
+else:
+    print(0)
