bbb7b244fdfd792ebf15ac1b8529dd9fd9c38f7e
vitcmaestro <40243064+vitcmaestro@users.noreply.github.com>
2019-03-06 22:30:48 +0530

    Update Print_str.py

--- Print_str.py
+++ Print_str.py
@@ -1,14 +1,16 @@
 st1 = input("")
 st2 = input("")
 j = 0
+li = []
 for i in range(min(len(st1),len(st2))):
-    print(st2[i]+st1[i])
+    li.append(st2[i]+st1[i])
     j+=1
 if(len(st1)>len(st2)):
     while(j<len(st1)):
-        print(st1[j])
+        li.append(st1[j])
         j+=1
 elif(len(st1)<len(st2)):
     while(j<len(st2)):
-        print(st2[j])
+        li.append(st2[j])
         j+=1
+print("\n".join(map(str,li)))
